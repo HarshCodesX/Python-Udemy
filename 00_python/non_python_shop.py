9eab962e24b5e9765f6da77e1242aefbfaf32134
gupta-harsh1 <gupta-harsh1@aramark.com>
2026-07-10 20:12:21 +0530

    added changes

---
+++ 00_python/non_python_shop.py
@@ -0,0 +1,13 @@
+class Chai:
+    def __init__(self, sweetness, milk_level):
+        self.sweetness = sweetness
+        self.milk_level = milk_level
+
+    def sip(self):
+        print("Sipping chai")
+
+    def add_sugar(self, amount):
+        print("added sugar")
+
+myChai = Chai(sweetness = 2.5, milk_level = 2)
+myChai.add_sugar(2)
