37b14e32c995d41b8d4d5c556c48614bf2a0a76c
gupta-harsh1 <gupta-harsh1@aramark.com>
2026-07-13 02:16:41 +0530

    started about dictionary in python

---
+++ 02_datatypes/chapter_10.py
@@ -0,0 +1,8 @@
+chai_order = dict(type="Masala Chai", size="Large", sugar = 2)
+print(chai_order)
+
+chai_recipe = {}
+chai_recipe["base"] = "water"
+chai_recipe["tea_type"] = "black tea"
+
+print(f"Chai Recipe: {chai_recipe}")
