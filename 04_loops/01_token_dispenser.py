a560fd42ba34b1f4bba810b1d63d7836ef6f7b14
gupta-harsh1 <gupta-harsh1@aramark.com>
2026-07-16 02:43:17 +0530

    started conditionals in python

---
+++ 04_loops/01_token_dispenser.py
@@ -0,0 +1,2 @@
+for token in range(1, 11):
+    print(f"Token which is going on is: {token}")
---
+++ 04_loops/tea_orders.py
@@ -0,0 +1,3 @@
+orders = ["Harsh", "Hitesh", "Becky", "Carlos"]
+for name in orders:
+    print(f"Order ready for {name}")
