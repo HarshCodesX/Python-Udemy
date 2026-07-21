3b2e225e76a36c9b2f931001b379a1cf5792f648
gupta-harsh1 <harshguptahb055@gmail.com>
2026-07-22 02:11:04 +0530

    more code

---
+++ 06_chai_business/main.py
@@ -0,0 +1,10 @@
+# import recipes.flavors
+# print(recipes.flavors.ginger_chai())
+
+# Another example
+
+# from recipes.flavors import cardamom_chai, ginger_chai
+# print(cardamom_chai())
+
+# Another example
+
---
+++ 06_chai_business/recipes/flavors.py
@@ -0,0 +1,5 @@
+def cardamom_chai():
+    return "cardamom tea is ready"
+
+def ginger_chai():
+    return "ginger tea is ready"
