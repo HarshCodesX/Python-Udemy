7e7fb2406ed7dc5b75eece29b29f588784486205
gupta-harsh1 <harshguptahb055@gmail.com>
2026-07-26 21:05:23 +0530

    started inheritance and comprehension in python

---
+++ 10_oops/06_inheritance_composition.py
@@ -0,0 +1,28 @@
+class BaseTea:
+    temperature = "hot"
+    def __init__(self, type_):
+        self.type = type_
+
+    def prepare(self):
+        print(f"Preparing {self.type} Tea!")
+
+class GingerTea(BaseTea):
+    def add_spices(self):
+        print(f"Adding cardamom, ginger and cloves")
+
+# spcl_tea = GingerTea("Lemon")
+# spcl_tea.prepare()
+# spcl_tea.add_spices()
+
+class TeaShop:
+    tea_class = BaseTea
+    def __init__(self):
+        self.tea = self.tea_class("Regular")
+
+    def serve(self):
+        print(f"Serving {self.tea.type} tea")
+        self.tea.prepare()
+
+class FancyTeaShop(TeaShop):
+    tea_cls = GingerTea
+    
