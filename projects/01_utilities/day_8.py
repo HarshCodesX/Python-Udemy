5b616014435e9b2951ca2d694f76feff78110593
gupta-harsh1 <harshguptahb055@gmail.com>
2026-07-30 20:16:03 +0530

    password strength checker

--- projects/01_utilities/day_8.py
+++ projects/01_utilities/day_8.py
@@ -26,4 +26,28 @@ def check_password_strength(password):
     issues = []
     if len(password) < 8:
         issues.append("Too short (minimum 8 characters)")
-    if not any()
+    if not any(c.islower for c in password):
+        issues.append("Missing lower case letter")
+    if not any(c.isupper() for c in password):
+        issues.append("Missing upper case letter")
+    if not any(c.isdigit() for c in password):
+        issues.append("Missing digit in password")
+    if not any(c in string.punctuation for c in password):
+        issues.append("Missing a special character")
+    return issues
+
+def generate_strong_password(length=12):
+    chars =  string.ascii_letters + string.digits + string.punctuation
+    return "".join(random.choice(chars) for _ in range(length))
+
+password = getpass.getpass("Enter a password: ")
+issues = check_password_strength(password)
+if not issues:
+    print("Strong password!")
+else:
+    print("weak password!")
+    for issue in issues:
+        print(issue)
+    suggestion = generate_strong_password()
+    print("\n Suggesting you a strong password")
+    print(suggestion)
