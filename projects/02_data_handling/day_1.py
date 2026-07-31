ed57bb0279192777ffd2a91857361224bb48619a
gupta-harsh1 <harshguptahb055@gmail.com>
2026-07-31 20:38:27 +0530

    added code related to project in python

--- projects/02_data_handling/day_1.py
+++ projects/02_data_handling/day_1.py
@@ -27,3 +27,54 @@ Bonus:
 - Allow partial match search
 - Prevent duplicate names from being added
 """
+
+import csv
+import os
+
+FILENAME = "contacts.csv"
+
+if not os.path.exists(FILENAME):
+    with open(FILENAME, "w", newline="", encoding="utf-8") as f:
+        writer = csv.writer(f)
+        writer.writerow(["Name", "Phone", "Email"])
+
+def add_contact():
+   name = input("Name: ").strip()
+   phone = input("Phone: ").strip()
+   email = input("Email: ").strip()
+
+   #check for duplicates
+   with open(FILENAME, 'r', encoding="utf-8") as f:
+      reader = csv.DictReader(f)
+      for row in reader:
+          if row["name"].lower() == name.lower():
+              print("Contact name already exists")
+              return
+   
+   with open(FILENAME, 'a', encoding="utf-8") as f:
+       writer = csv.writer(f)
+       writer.writerow([name, phone, email])
+       print("Contact added")
+
+def view_contacts():
+    with open(FILENAME, 'r', encoding="utf-8") as f:
+        reader = csv.reader(f)
+        rows = list(reader)
+        if len(rows) < 1:
+            print("no contacts found")
+            return
+        print("\nYour contacts: \n")
+        for row in rows[1:]: #to start from second row as rows[0] is the heading - name, phone, email
+            print(f"{row[0]} | {row[1]} | {row[2]}")
+        print()
+
+def search_contact():
+    term = input("Enter the name to search: ").strip().lower()
+    found = False
+    with open(FILENAME, 'r', encoding="utf-8") as f:
+        reader = csv.DictReader(f)
+
+       
+
+   
+
