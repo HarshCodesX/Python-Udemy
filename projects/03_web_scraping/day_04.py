38a95485c6c6e936eb7983afb98bbab7a6c65476
gupta-harsh1 <harshguptahb055@gmail.com>
2026-08-10 21:47:34 +0530

    scraped multiple pages

---
+++ projects/03_web_scraping/day_04.py
@@ -0,0 +1,22 @@
+"""
+ Challenge: Download Cover Images of First 10 Books
+
+Goal:
+- Visit https://books.toscrape.com/
+- Scrape the first 10 books listed on the homepage
+- For each book, extract:
+  • Title
+  • Image URL
+
+Then:
+- Download each image
+- Save it to a local `images/` folder with the filename as the book title (sanitized)
+
+Example:
+ Title: "A Light in the Attic"
+ Saved as: images/A_Light_in_the_Attic.jpg
+
+Bonus:
+- Handle invalid filename characters
+- Show download progress
+"""
