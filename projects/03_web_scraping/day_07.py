ce55c15eb6164086a3cc627624435bfa6996edd9
gupta-harsh1 <harshguptahb055@gmail.com>
2026-08-11 17:24:01 +0530

    added image creator in python

---
+++ projects/03_web_scraping/day_07.py
@@ -0,0 +1,11 @@
+"""
+ Challenge: Crypto Price Tracker with Graphs
+
+Goal:
+- Fetch live prices of the top 10 cryptocurrencies using CoinGecko's free public API
+- Store prices in a CSV file with timestamp
+- Generate a line graph for a selected coin over time (price vs. time)
+- Repeatable — user can run this multiple times to log data over time
+
+JSON handling, API usage, CSV storage, matplotlib graphing
+"""
