658dc89e5be18ce3a0d4b1db09da873a9c407985
gupta-harsh1 <harshguptahb055@gmail.com>
2026-08-13 17:59:39 +0530

    added code for routes

--- projects/06_url_shortner/app.py
+++ projects/06_url_shortner/app.py
@@ -1,11 +1,23 @@
-from flask import Flask
+from flask import Flask, request, redirect, render_template
+import random
+import string
 from models import (init_db, insert_url, get_url, get_all_urls, increment_visit_count, delete_url_by_code)
 
 app = Flask(__name__)
+init_db()
 
-@app.route("/")
-def hello_world():
-    return 'hello'
+def generate_short_code(length=6):
+    return ''.join(random.choice(string.ascii_letters + string.digits, k=length))
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        original_url = request.form['url']
+        short_code = generate_short_code(original_url)
+        insert_url(original_url, short_code)
+        return redirect("/")
+    all_urls = get_all_urls()
+    return render_template('index.html', all_urls=all_urls)
 
 @app.route("/about")
 def about():
--- projects/06_url_shortner/models.py
+++ projects/06_url_shortner/models.py
@@ -8,7 +8,7 @@ def init_db():
             CREATE TABLE IF NOT EXISTS urls(
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 original_url TEXT NOT NULL,
-                short_code TEXT UNIQUE NOT NULL
+                short_code TEXT UNIQUE NOT NULL,
                 visit_count INTEGER DEFAULT 0
             )
     ''')
