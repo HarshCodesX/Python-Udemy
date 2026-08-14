72d888b7b9b633074cd26009498201341ab32bb6
gupta-harsh1 <harshguptahb055@gmail.com>
2026-08-14 18:38:00 +0530

    code related to concurrency and multi-threading

---
+++ 12_threads_concurrency/02_multiprocession.py
@@ -0,0 +1,23 @@
+from multiprocessing import Process
+import time
+
+def brew_tea(name):
+    print(f"Start of {name} tea brewing")
+    time.sleep(3)
+    print(f"End of {name} tea brewing")
+
+if __name__ == "__main__":
+    tea_makers = [
+        Process(target=brew_tea, args=(f"Tea Maker #{i + 1}", ))
+        for i in range(3)
+    ]
+
+    #start all process
+    for p in tea_makers:
+        p.start()
+
+    #wait for all to complete
+    for p in tea_makers:
+        p.join()
+
+    print("All tea served")
